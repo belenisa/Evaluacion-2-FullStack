682f1307a5b2a5c09d221ff4232de879afe38de1
belenisa <bel.alarconh@duocuc.cl>
2026-09-03 22:01:19 +0000

    Configurar GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Evaluacion-2-FullStack-main/'
+  base: '/Evaluacion-2-FullStack/'
 })
